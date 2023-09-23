2fba85b303ddb28e80b855cf10f0c72cc48ec253
Pham Tu <tubackkhoa@gmail.com>
2023-09-23 18:31:33 +0700

    add frontend

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.ts?$": ["esbuild-jest", { isolatedModules: true }]
-  },
-  testPathIgnorePatterns: ["build/", "node_modules/"]
-};
