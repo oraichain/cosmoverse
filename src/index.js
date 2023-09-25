cd57e6e183b27a94f7ae0953912f5e87e6ae19d4
Pham Tu <tubackkhoa@gmail.com>
2023-09-25 12:03:38 +0700

    add multiple notebook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
+import { createRoot } from "react-dom/client";
+import Select from "react-select";
+
 import nb from "./notebook";
 import "./index.css";
+import { useEffect, useRef, useState } from "react";
 
-fetch("/readme.ipynb")
-  .then((res) => res.json())
-  .then((json) => {
+const options = [
+  { value: "/basic.ipynb", label: "Basic interactions" },
+  { value: "/ibc.ipynb", label: "IBC channels" }
+];
+
+const App = () => {
+  const [value, setValue] = useState();
+  const ref = useRef();
+
+  useEffect(() => {
+    changeNotebook(options[0]);
+  }, []);
+
+  const runAll = async () => {
+    for (const run of runCodes) await run();
+  };
+
+  const changeNotebook = async (item) => {
+    setValue(item);
+    const json = await fetch(item.value).then((res) => res.json());
     const notebook = nb.parse(json);
-    document.querySelector("#root").appendChild(notebook.render());
-  });
+    ref.current.innerHTML = "";
+    ref.current.appendChild(notebook.render());
+  };
+
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Select autosize options={options} value={value} onChange={changeNotebook} />
+        <button onClick={runAll}>Run all</button>
+      </div>
+      <div ref={ref} />
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
